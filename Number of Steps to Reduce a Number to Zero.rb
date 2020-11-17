858321039427c152a1a5921f219071fffa77e0d4
Grecia RO <56781403+greciaro@users.noreply.github.com>
2020-11-17 15:37:51 -0800

    Add files via upload

---
+++ Number of Steps to Reduce a Number to Zero.rb
@@ -0,0 +1,15 @@
+# @param {Integer} num
+# @return {Integer}
+def number_of_steps (num)
+    count = 0
+    while num > 0
+        if num.even?
+            num /= 2
+            count += 1
+        else
+            num -= 1
+             count += 1
+        end
+    end
+        return count
+end
