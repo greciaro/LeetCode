768e5a15bbf36bfb7aeb41b9e0f8c6c684415d1d
Grecia RO <56781403+greciaro@users.noreply.github.com>
2020-11-24 17:43:05 -0800

    Add files via upload

---
+++ 1512.rb
@@ -0,0 +1,44 @@
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+# @param {Integer[]} nums
+# @return {Integer}
+def num_identical_pairs(nums)
+    count = 0
+
+nums.each.with_index do |ele, i|
+    nums.each.with_index do |ele2, j|
+        
+        count += 1 if (ele == ele2) && (i < j)
+        
+    end
+end
+
+count
+end
