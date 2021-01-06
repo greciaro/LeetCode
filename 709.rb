f3ddb95196b84457ab53be60ef77a8333ba05e93
Grecia RO <56781403+greciaro@users.noreply.github.com>
2021-01-05 19:50:57 -0800

    Add files via upload

--- 709.rb
+++ 709.rb
@@ -14,3 +14,20 @@ Example 3:
 
 Input: "LOVELY"
 Output: "lovely"
+
+# @param {String} str
+# @return {String}
+def to_lower_case(str)
+arr = str.split("")
+alpha_low = ('a'...'z').to_a.join
+alpha_up = alpha_low.upcase 
+
+
+arr.each_with_index do |ele, i|
+    if alpha_up.include?(ele) 
+        arr[i] = alpha_low[alpha_up.index(ele)]
+    end
+end
+
+arr.join("")  
+end
