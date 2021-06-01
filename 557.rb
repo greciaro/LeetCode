e0b61a1b4b0088981dd6d391fdae1223ccd65af4
Grecia RO <56781403+greciaro@users.noreply.github.com>
2021-06-01 13:13:05 -0700

    Add files via upload

--- 557.rb
+++ 557.rb
@@ -19,3 +19,19 @@ s contains printable ASCII characters.
 s does not contain any leading or trailing spaces.
 There is at least one word in s.
 All the words in s are separated by a single space.
+
+
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+    array = s.split(" ")
+new_array = []
+
+array.each do |word|
+
+   new_array << word.reverse!
+  
+end
+
+new_array.join(" ")
+end
